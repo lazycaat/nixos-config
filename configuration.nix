d49afaf2e5ef5c44bbe37eaf347821e6bbb0310f
lazycat <lazycat@riseup.net>
2024-03-22 15:15:18 +0300

    test3

--- configuration.nix
+++ configuration.nix
@@ -55,31 +55,31 @@
     umount /btrfs_tmp
   '';
 
-  fileSystems."/persist".neededForBoot = true;
-  environment.persistence."/persist/system" = {
-    hideMounts = true;
-    directories = [
-      "/etc/nixos"
-      "/var/log"
-      "/var/lib/bluetooth"
-      "/var/lib/nixos"
-      "/var/lib/systemd/coredump"
-      "/etc/NetworkManager/system-connections"
-      { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
-    ];
-    files = [
-      "/etc/machine-id"
-      { file = "/var/keys/secret_file"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
-    ];
-  };
+  # fileSystems."/persist".neededForBoot = true;
+  # environment.persistence."/persist/system" = {
+  #   hideMounts = true;
+  #   directories = [
+  #     "/etc/nixos"
+  #     "/var/log"
+  #     "/var/lib/bluetooth"
+  #     "/var/lib/nixos"
+  #     "/var/lib/systemd/coredump"
+  #     "/etc/NetworkManager/system-connections"
+  #     { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
+  #   ];
+  #   files = [
+  #     "/etc/machine-id"
+  #     { file = "/var/keys/secret_file"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
+  #   ];
+  # };
 
-  programs.fuse.userAllowOther = true;
-  home-manager = {
-    extraSpecialArgs = {inherit inputs;};
-    users = {
-      "user1" = import ./home.nix;
-    };
-  };
+  # programs.fuse.userAllowOther = true;
+  # home-manager = {
+  #   extraSpecialArgs = {inherit inputs;};
+  #   users = {
+  #     "user1" = import ./home.nix;
+  #   };
+  # };
 
     nix.settings.experimental-features = [
     "nix-command"
