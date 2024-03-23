3756da4fbd686b4d2de0e455ca035ffe79433e42
lazycat <lazycat@riseup.net>
2024-03-23 23:10:49 +0300

    test30

--- home/module/module/stylix/default.nix
+++ home/module/module/stylix/default.nix
@@ -5,11 +5,11 @@
 }: let
 
   theme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
-  wallpaper = pkgs.runCommand "image.png" {} ''
-        COLOR=$(${pkgs.yq}/bin/yq -r .base00 ${theme})
-        COLOR="#"$COLOR
-        ${pkgs.imagemagick}/bin/magick convert -size 1920x1080 xc:$COLOR $out
-  '';
+  # wallpaper = pkgs.runCommand "image.png" {} ''
+  #       COLOR=$(${pkgs.yq}/bin/yq -r .base00 ${theme})
+  #       COLOR="#"$COLOR
+  #       ${pkgs.imagemagick}/bin/magick convert -size 1920x1080 xc:$COLOR $out
+  # '';
 
 in {
 
@@ -18,7 +18,7 @@ in {
   ];
 
   stylix = {
-    image = wallpaper;
+    # image = wallpaper;
     base16Scheme = theme;
     # polarity = "dark";
     autoEnable = true;
