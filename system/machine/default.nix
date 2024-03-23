b6939286ad190aa55dae7c0aa8cc7147799e2490
lazycat <lazycat@riseup.net>
2024-03-23 21:06:11 +0300

    test20

--- system/machine/default.nix
+++ system/machine/default.nix
@@ -1,18 +1,21 @@
 {
   inputs,
+  self,
   ...
-}:
+}: let
+  inherit (inputs.nixpkgs.lib) nixosSystem;
+  specialArgs = {inherit inputs self;};
 
-{
+in {
 
   # NixOS configurations
-  parts.nixosConfigurations = {
+  flake.nixosConfigurations = {
 
-    machine1 = {
+    machine1 = nixosSystem {
+      inherit specialArgs;
       system = "x86_64-linux";
       stateVersion = "23.11"; # only change this if you know what you are doing.
 
-      specialArgs = { inherit inputs; };
       modules = [
         ./machine1
       ];
