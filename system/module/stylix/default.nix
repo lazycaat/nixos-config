950c544e02d277b15cf4688e10ca26333b347408
lazycat <lazycat@riseup.net>
2024-03-24 11:35:01 +0300

    test46

--- system/module/stylix/default.nix
+++ system/module/stylix/default.nix
@@ -6,9 +6,10 @@
 
   theme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
   wallpaper = pkgs.runCommand "image.png" {} ''
-        COLOR=$(${pkgs.yq}/bin/yq -r .base00 ${theme})
-        COLOR="#"$COLOR
-        ${pkgs.imagemagick}/bin/magick convert -size 1x1 xc:$COLOR $out
+        # COLOR=$(${pkgs.yq}/bin/yq -r .base00 ${theme})
+        # COLOR="#"$COLOR
+        # ${pkgs.imagemagick}/bin/magick convert -size 1x1 xc:$COLOR $out
+        ${pkgs.imagemagick}/bin/magick convert -size 1x1 xc:black $out
   '';
 
 in {
