25ca2ee652f76d19bebf72b278d11ed55b8ad7e0
lazycat <lazycat@riseup.net>
2024-04-05 22:46:18 +0300

    test87

--- system/machine/android1/home/default.nix
+++ system/machine/android1/home/default.nix
@@ -12,20 +12,20 @@
   imports = [ 
 
     # apps
-    ../../../home/module/app/terminal/broot
-    ../../../home/module/app/terminal/btop
-    ../../../home/module/app/terminal/fish
-    ../../../home/module/app/terminal/helix
-    ../../../home/module/app/terminal/lsd
-    ../../../home/module/app/terminal/ripgrep
-    ../../../home/module/app/terminal/skim
-    ../../../home/module/app/terminal/yazi
-    ../../../home/module/app/terminal/zellij
-    ../../../home/module/app/terminal/zoxide
+    ../../../../home/module/app/terminal/broot
+    ../../../../home/module/app/terminal/btop
+    ../../../../home/module/app/terminal/fish
+    ../../../../home/module/app/terminal/helix
+    ../../../../home/module/app/terminal/lsd
+    ../../../../home/module/app/terminal/ripgrep
+    ../../../../home/module/app/terminal/skim
+    ../../../../home/module/app/terminal/yazi
+    ../../../../home/module/app/terminal/zellij
+    ../../../../home/module/app/terminal/zoxide
 
     # modules
     # "${module}/stylix"
-    ../../../home/module/module/nix-index
+    ../../../../home/module/module/nix-index
 
     # per user
     ./variables.nix
